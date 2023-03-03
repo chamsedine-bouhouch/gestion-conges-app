ac7c188ca2f6328fbdc4b72de87b25253277add3
chamsedine-bouhouch <bouhouchchamseddine@gmail.com>
2023-03-03 21:36:41 +0100

    Leave model and migration setup

---
+++ database/migrations/2023_03_03_202334_create_leaves_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('leaves', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp('start_date');
+            $table->timestamp('end_date');
+            $table->foreignId('user_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('leaves', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+        });
+        Schema::dropIfExists('leaves');
+    }
+};
