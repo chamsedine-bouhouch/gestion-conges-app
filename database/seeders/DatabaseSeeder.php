e6260e5111c6028b5f096d2c11fc9836dbbdc94f
chamsedine-bouhouch <bouhouchchamseddine@gmail.com>
2023-03-03 21:53:46 +0100

    add users & leaves data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +15,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        DB::table('leaves')->insert([
+            'start_date' => Carbon::now(),
+            'end_date' => Carbon::now()->addWeeks(1),
+            'user_id' => 2
+        ]);
+
+        DB::table('leaves')->insert([
+            'start_date' => Carbon::now()->addWeeks(1),
+            'end_date' => Carbon::now()->addWeeks(3),
+            'user_id' => 2
+        ]);
+
     }
 }
